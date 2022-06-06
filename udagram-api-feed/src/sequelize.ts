172c68434ebd30e7d8b8938c6b3622d6e8454d4f
Orim Dominic <orimdominic@gmail.com>
2022-06-06 10:00:51 +0100

    fix: errors preventing build

--- udagram-api-feed/src/sequelize.ts
+++ udagram-api-feed/src/sequelize.ts
@@ -1,4 +1,4 @@
-import { Sequelize } from "sequelize-typescript";
+import { ISequelizeConfig, Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
 export const sequelize = new Sequelize({
@@ -9,4 +9,4 @@ export const sequelize = new Sequelize({
 
   dialect: config.dialect,
   storage: ":memory:",
-});
+} as ISequelizeConfig);
