1e8c8cfa57b635152953be80ee67e67e0018acba
Orim Dominic <orimdominic@gmail.com>
2022-06-06 05:05:03 +0100

    chore: disable monolith server

--- udagram-api/src/server.ts
+++ udagram-api/src/server.ts
@@ -47,8 +47,8 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   });
 
   // Start the Server
-  app.listen(port, () => {
-    console.log(`server running ${config.url}`);
-    console.log(`press CTRL+C to stop server`);
-  });
+  // app.listen(port, () => {
+  //   console.log(`server running ${config.url}`);
+  //   console.log(`press CTRL+C to stop server`);
+  // });
 })();
