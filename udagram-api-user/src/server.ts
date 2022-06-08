f479ead79c9af48ef44cddd4ecf73db9d48318bf
Orim Dominic <orimdominic@gmail.com>
2022-06-08 07:14:00 +0100

    debug: api-user

--- udagram-api-user/src/server.ts
+++ udagram-api-user/src/server.ts
@@ -12,6 +12,7 @@ import { V0_USER_MODELS } from "./controllers/v0/model.index";
   await sequelize.addModels(V0_USER_MODELS);
 
   console.debug("Initialize database connection...");
+  console.debug("CONFIG", config);
   await sequelize["sync"]();
 
   const app = express();
