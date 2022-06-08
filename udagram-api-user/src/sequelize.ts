f3c6affaa702f07ab5cac84fb7f17633a504769b
Orim Dominic <orimdominic@gmail.com>
2022-06-08 06:56:53 +0100

    debug: check db vals

--- udagram-api-user/src/sequelize.ts
+++ udagram-api-user/src/sequelize.ts
@@ -1,10 +1,5 @@
 import { ISequelizeConfig, Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
-console.log(
-  'username:', config.username,
-  'password:',config.password,
-  'database:',config.database
-);
 
 export const sequelize = new Sequelize({
   username: config.username,
